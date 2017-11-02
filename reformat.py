5a41d59ecc7ee99e29099aa4d4e71ce8f99e510c
Daniel Zheng <dzheng256@gmail.com>
2017-11-02 15:36:32 -0400

    fixed reformat.py, update json files

--- reformat.py
+++ reformat.py
@@ -1,16 +1,13 @@
-import json
 import glob
 
 
-template = {'articles':[]}
-for filename in glob.glob('data/wiki_*'):
+for filename in glob.glob('data/wiki*'):
     with open(filename, 'r+') as f:
         s = f.read()
         s = s.replace('}', '},', s.count('}')-1) # replace all but last one to form valid json
         s = s.replace('\n', '')
-        s = "{'articles': [" + s + "]}"
-        json.dumps(f,s)
+        s = '{"articles": [' + s + ']}'
+        print(s)
+        with open(filename+'.json', 'w') as f2:
+            f2.write(s)
         print(filename)
-    break
-
-
